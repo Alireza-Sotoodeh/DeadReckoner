3f6cd2fe8042a620233e22d35911d10147c67d27
Alireza Sotoodeh <123328072+Alireza-Sotoodeh@users.noreply.github.com>
2025-07-23 20:50:15 +0330

    05/01-8:49
    first step
    needs improvments

--- Navigationa_PJ/Debug/Core/Src/main.d
+++ Navigationa_PJ/Debug/Core/Src/main.d
@@ -25,9 +25,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/driver_mpu6500.h ../Core/Inc/driver_mpu6500_dmp.h \
- ../Core/Inc/driver_mpu6500_interface.h ../Core/Inc/driver_mpu6500.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -56,7 +54,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
-../Core/Inc/driver_mpu6500.h:
-../Core/Inc/driver_mpu6500_dmp.h:
-../Core/Inc/driver_mpu6500_interface.h:
-../Core/Inc/driver_mpu6500.h:
