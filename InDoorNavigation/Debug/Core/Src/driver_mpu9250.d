5e9d19fe4d092ecb3c9242f30b9ba74343460a3b
Alireza Sotoodeh <123328072+Alireza-Sotoodeh@users.noreply.github.com>
2025-08-04 09:02:54 +0330

    start

--- InDoorNavigation/Debug/Core/Src/driver_mpu9250.d
+++
@@ -1,4 +0,0 @@
-Core/Src/driver_mpu9250.o: ../Core/Src/driver_mpu9250.c \
- ../Core/Inc/driver_mpu9250.h ../Core/Inc/driver_mpu9250_code.h
-../Core/Inc/driver_mpu9250.h:
-../Core/Inc/driver_mpu9250_code.h:
--- InDoorNavigation/Debug/Core/Src/driver_mpu9250_dmp_read_test.d
+++
@@ -1,7 +0,0 @@
-Core/Src/driver_mpu9250_dmp_read_test.o: \
- ../Core/Src/driver_mpu9250_dmp_read_test.c \
- ../Core/Inc/driver_mpu9250_dmp_read_test.h \
- ../Core/Inc/driver_mpu9250_interface.h ../Core/Inc/driver_mpu9250.h
-../Core/Inc/driver_mpu9250_dmp_read_test.h:
-../Core/Inc/driver_mpu9250_interface.h:
-../Core/Inc/driver_mpu9250.h:
